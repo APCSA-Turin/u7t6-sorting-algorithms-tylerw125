a463b6f0d60781c61509b576ff30f004754ff4c0
tylerw125 <tylerw125@nycstudents.net>
2025-02-26 13:16:55 +0000

    Fix conflicts

--- src/main/java/com/example/project/Selection_Sort/Insertion_Sort/InsertionSort.java
+++ src/main/java/com/example/project/Selection_Sort/Insertion_Sort/InsertionSort.java
@@ -2,26 +2,32 @@ package com.example.project.Selection_Sort.Insertion_Sort;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class InsertionSort {
 
     // PART A. implementing insertion sort
     public static int[] insertionSort(int[] elements) {
-        for (int i = 0; i < elements.length - 1; i++) {
-            while (elements[i] < elements[i + 1]) {
-                int temp = elements[i + 1];
-                elements[i + 1] = elements[i];
-                elements[i] = temp;
+        for (int i = 1; i < elements.length; i++) {
+            int key = elements[i];  // element to be inserted
+            int j = i - 1;
+
+            // MOve elements to the right to make space for key
+            while (j >= 0 && elements[j] > key) {
+                elements[j + 1] = elements[j];
+                j--;
             }
+            elements[j + 1] = key; // Insert key at other position
         }
         return elements;
     }
+
     public static void main(String[] args) {
         int[] list = {5,4,3,2,1};
-        System.out.println("Before: " + list.toString());
+        System.out.println("Before: " + Arrays.toString(list));
         InsertionSort.insertionSort(list);
-        System.out.println("After: " + list.toString());
+        System.out.println("After: " + Arrays.toString(list));
     }
 
     public static void selectionSort(int[] elements) {
